bdd075eb83cc09c2da45f8e8e5cd87206e1ed085
Rocktim Saikia <srocktim61@gmail.com>
2021-05-08 09:44:08 +0530

    feat: change the project structure

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 type Mapper<Element = any, NewElement = unknown> = (
   element: Element,
   index: number
-) => NewElement | Promise<NewElement>;
+) => NewElement | Promise<NewElement>
 
 const pMap = async <T, N>(array: Array<T>, mapper: Mapper<T, N>) => {
-  const promiseArray = array.map(async (item, index) => {
-    const current = await Promise.resolve(item);
-    return mapper(current, index);
-  });
-  return Promise.all(promiseArray);
-};
+  const promiseArray = array.map(async(item, index) => {
+    const current = await Promise.resolve(item)
+    return mapper(current, index)
+  })
+  return Promise.all(promiseArray)
+}
 
-export default pMap;
+export default pMap
